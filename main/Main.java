a5c6d7d31cb1be25bdcf2c97477da62300acd1c0
3A <antonioandroid3a@gmail.com>
2022-08-17 09:40:44 +0100

    main files

---
+++ main/Main.java
@@ -0,0 +1,12 @@
+/**
+ ** Code by: "António 3A"
+ ** :
+**/
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
---
+++ main/main.cpp
@@ -0,0 +1,27 @@
+/**
+ ** Code by: "António 3A"
+ ** :
+**/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fIO ios::sync_with_stdio(0); cin.tie(0);
+#define in freopen("input.in", "r", stdin);
+#define out freopen("output.out", "w", stdout);
+#define ccl(i, begin, end) for(auto i = begin; i < end; i ++)
+
+typedef long long ll;
+
+int main()
+{
+    fIO
+    //in
+    //out
+
+    
+    return 0;
+}
